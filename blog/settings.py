2274252ad38a90ad76fd27201feb659646d7d93e
dilanelefa <lefakongdilane@gmail.com>
2024-04-05 10:48:56 +0100

    header css and jss

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7s4sdo&r^dd=nzc7zc5&bcqu^enggfiv+qr&^3mg$%3_#$6=)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["blog-test-wa18.onrender.com"]
+ALLOWED_HOSTS = ["blog-test-wa18.onrender.com", "localhost",]
 
 # Application definition
 
